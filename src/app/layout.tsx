bc2f113529d9ad5f76a91fcb880e63957814acbe
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-10-01 17:39:46 -0300

    feat(layout): add header and footer to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,7 @@
 import "./styles/globals.css";
+import { inter } from "./fonts/inter";
+import { Header } from "./components/header";
+import { Footer } from "./components/footer";
 
 export default function RootLayout({
   children,
@@ -7,7 +10,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-gray-950 antialiased">{children}</body>
+      <body className={`${inter.variable} bg-gray-950 antialiased`}>
+        <div className="flex flex-col gap-6">
+          <Header />
+
+          {children}
+
+          <Footer />
+        </div>
+      </body>
     </html>
   );
 }
