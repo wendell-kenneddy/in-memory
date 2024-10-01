e740faaf9e6994589e5e2672754a61a824d4a8c5
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-10-01 15:10:18 -0300

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+import "./styles/globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="bg-gray-950 antialiased">{children}</body>
+    </html>
+  );
+}
