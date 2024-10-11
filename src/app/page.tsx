7e9fd9777e280e2b41baab8be2f96148e0da5e78
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-10-11 14:15:16 -0300

    feat(pages/home): add homepage content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import { Metadata } from "next";
+import { Tabs } from "./components/tabs";
+import { GameMenu } from "./components/game-menu";
+import { Leaderboard } from "./components/leaderboard";
+
+export const metadata: Metadata = {
+  title: "in.memory",
+  description: "Test your memorization skills with in.memory!",
+};
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    <main className="w-11/12 max-w-[720px] mx-auto">
+      <Tabs
+        entries={[
+          { value: "Play", component: <GameMenu /> },
+          { value: "Leaderboard", component: <Leaderboard /> },
+        ]}
+        defaultValue="Play"
+      />
+    </main>
   );
 }
