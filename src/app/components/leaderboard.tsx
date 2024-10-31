4c5b5c68b706231420a63f1682f33d5a15305137
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-10-31 19:40:19 -0300

    refactor(components/leaderboard): remove clear time prop from entries

--- src/app/components/leaderboard.tsx
+++ src/app/components/leaderboard.tsx
@@ -8,30 +8,21 @@ export function Leaderboard() {
           <th className="py-2 bg-gray-900 rounded-tl-md">Rank</th>
           <th className="py-2 bg-gray-900">Name</th>
           <th className="py-2 bg-gray-900">Level</th>
-          <th className="py-2 bg-gray-900 rounded-tr-md">Time</th>
         </tr>
       </thead>
 
       <tbody>
-        <LeaderboardEntry rank={1} name="Adam" level={19} clearTime="01:30" />
-        <LeaderboardEntry rank={1} name="Adam" level={19} clearTime="01:30" />
-        <LeaderboardEntry rank={1} name="Adam" level={19} clearTime="01:30" />
-        <LeaderboardEntry rank={1} name="Adam" level={19} clearTime="01:30" />
-        <LeaderboardEntry rank={1} name="Adam" level={19} clearTime="01:30" />
-        <LeaderboardEntry rank={1} name="Adam" level={19} clearTime="01:30" />
-        <LeaderboardEntry rank={1} name="Adam" level={19} clearTime="01:30" />
-        <LeaderboardEntry rank={1} name="Adam" level={19} clearTime="01:30" />
-        <LeaderboardEntry rank={1} name="Adam" level={19} clearTime="01:30" />
-        <LeaderboardEntry rank={1} name="Adam" level={19} clearTime="01:30" />
-        <LeaderboardEntry rank={1} name="Adam" level={19} clearTime="01:30" />
-        <LeaderboardEntry rank={1} name="Adam" level={19} clearTime="01:30" />
+        <LeaderboardEntry rank={1} name="Adam" level={99} />
+        <LeaderboardEntry rank={2} name="Odysseus" level={98} />
+        <LeaderboardEntry rank={3} name="Calypso" level={97} />
+        <LeaderboardEntry rank={4} name="Eurilochus" level={96} />
+        <LeaderboardEntry rank={5} name="Persefone" level={95} />
+        <LeaderboardEntry rank={6} name="Hades" level={94} />
+        <LeaderboardEntry rank={7} name="Ares" level={93} />
+        <LeaderboardEntry rank={8} name="Athena" level={92} />
+        <LeaderboardEntry rank={9} name="Hermes" level={91} />
+        <LeaderboardEntry rank={10} name="Zeus" level={90} />
       </tbody>
-
-      <tfoot>
-        <tr className="text-center text-gray-100/60">
-          <td colSpan={4}>Oredered by highest level and lowest clear time.</td>
-        </tr>
-      </tfoot>
     </table>
   );
 }
