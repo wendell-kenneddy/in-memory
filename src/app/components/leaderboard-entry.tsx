d4bd5a66ed59126f2ae155c99fe1e9b3e3b95e73
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-10-31 19:39:54 -0300

    refactor(components/leaderboard-entry): remove clear time

--- src/app/components/leaderboard-entry.tsx
+++ src/app/components/leaderboard-entry.tsx
@@ -2,16 +2,14 @@ interface LeaderboardEntryProps {
   rank: number;
   name: string;
   level: number;
-  clearTime: string;
 }
 
-export function LeaderboardEntry({ rank, name, level, clearTime }: LeaderboardEntryProps) {
+export function LeaderboardEntry({ rank, name, level }: LeaderboardEntryProps) {
   return (
     <tr className="text-center even:bg-gray-800 odd:bg-gray-700">
       <td className="py-2">{rank}</td>
       <td className="py-2">{name}</td>
       <td className="py-2">{level}</td>
-      <td className="py-2">{clearTime}</td>
     </tr>
   );
 }
