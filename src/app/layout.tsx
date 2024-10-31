c404e7c2436d09af6c8b6a309722167f594030b8
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-10-31 19:40:53 -0300

    feat(layout): add icon and theme tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,20 @@
 import "./styles/globals.css";
+import "@radix-ui/themes/styles.css";
 import { inter } from "./fonts/inter";
 import { Header } from "./components/header";
 import { Footer } from "./components/footer";
+import { Theme } from "@radix-ui/themes";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  creator: "Wendell Kenneddy",
+  icons: [
+    { rel: "icon", url: "/favicon.ico", sizes: "any" },
+    { rel: "icon", url: "/icon.svg", type: "image/svg+xml" },
+    { rel: "apple-touch-icon", url: "/apple-touch-icon.png", type: "image/png" },
+  ],
+  other: { "theme-color": "#111827" },
+};
 
 export default function RootLayout({
   children,
@@ -10,14 +23,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} bg-gray-950 antialiased`}>
-        <div className="flex flex-col gap-6">
-          <Header />
+      <body>
+        <Theme appearance="dark" className={`${inter.variable} bg-gray-950 antialiased`}>
+          <div className="flex flex-col gap-6">
+            <Header />
 
-          {children}
+            {children}
 
-          <Footer />
-        </div>
+            <Footer />
+          </div>
+        </Theme>
       </body>
     </html>
   );
