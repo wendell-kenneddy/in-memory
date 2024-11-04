6d64d75cca2dab6a6338ec1df8be9a42c3b3cf64
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-11-04 09:05:45 -0300

    feat(components/header): remove login cto

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -4,14 +4,10 @@ import Link from "next/link";
 export function Header() {
   return (
     <header className="w-full bg-gray-900 py-4 shadow-md">
-      <div className="w-11/12 max-w-[720px] mx-auto flex items-center justify-between">
+      <div className="w-11/12 max-w-[720px] mx-auto flex items-center justify-center">
         <Link href="/">
           <Image src="/in-memory-logo.svg" alt="Logo" width={83} height={18} />
         </Link>
-
-        <div className="flex items-center gap-1">
-          <span className="font-medium text-gray-100/60 hover:text-gray-100">Login</span>
-        </div>
       </div>
     </header>
   );
